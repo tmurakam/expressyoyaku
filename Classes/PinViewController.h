e53d5f165835699abbd7364373512f37ab8c7535
Takuya Murakami <tmurakam@tmurakam.org>
2009-05-01 11:27:26 +0900

    add PinViewController

---
+++ Classes/PinViewController.h
@@ -0,0 +1,77 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+/*
+  CashFlow for iPhone/iPod touch
+
+  Copyright (c) 2008, Takuya Murakami, All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer. 
+
+  2. Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution. 
+
+  3. Neither the name of the project nor the names of its contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission. 
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// PIN code view
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@class PinViewController;
+
+@protocol PinViewDelegate
+- (void)pinViewFinished:(PinViewController *)vc isCancel:(BOOL)isCancel;
+@end
+
+@interface PinViewController : UIViewController 
+{
+    IBOutlet UILabel *valueLabel;
+
+    IBOutlet UIButton *button_Clear;
+    IBOutlet UIButton *button_BS;
+    IBOutlet UIButton *button_0;
+    IBOutlet UIButton *button_1;
+    IBOutlet UIButton *button_2;
+    IBOutlet UIButton *button_3;
+    IBOutlet UIButton *button_4;
+    IBOutlet UIButton *button_5;
+    IBOutlet UIButton *button_6;
+    IBOutlet UIButton *button_7;
+    IBOutlet UIButton *button_8;
+    IBOutlet UIButton *button_9;
+
+    NSMutableString *value;
+    BOOL enableCancel;
+    id<PinViewDelegate> delegate;
+}
+
+@property(nonatomic,assign) id<PinViewDelegate> delegate;
+@property(nonatomic,readonly) NSString *value;
+@property(nonatomic,assign) BOOL enableCancel;
+
+//- (IBAction)onNumButtonDown:(id)sender;
+- (IBAction)onNumButtonPressed:(id)sender;
+- (void)doneAction:(id)sender;
+- (void)cancelAction:(id)sender;
+
+@end
---
+++ Classes/PinViewController.m
@@ -0,0 +1,139 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+/*
+  EX Yoyaku Browser for iPhone/iPod touch
+
+  Copyright (c) 2009, Takuya Murakami, All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer. 
+
+  2. Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution. 
+
+  3. Neither the name of the project nor the names of its contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission. 
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+//#import <AudioToolbox/AudioToolbox.h>
+
+#import "PinViewController.h"
+
+@implementation PinViewController
+@synthesize value, enableCancel, delegate;
+
+- (id)init
+{
+    self = [super initWithNibName:@"PinView" bundle:nil];
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    value = [[NSMutableString alloc] init];
+
+    //self.title = NSLocalizedString(@"PIN", @"");
+    self.navigationItem.rightBarButtonItem = 
+        [[[UIBarButtonItem alloc]
+             initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+             target:self
+             action:@selector(doneAction:)] autorelease];
+
+    self.navigationItem.leftBarButtonItem = nil;
+    if (enableCancel) {
+        self.navigationItem.leftBarButtonItem = 
+            [[[UIBarButtonItem alloc]
+                 initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
+                 target:self
+                 action:@selector(cancelAction:)] autorelease];
+    }
+}
+
+- (void)dealloc
+{
+    [value release];
+    [super dealloc];
+}
+
+#if 0
+- (IBAction)onNumButtonDown:(id)sender
+{
+    // play keyboard click sound
+    AudioServicesPlaySystemSound(1105);
+}
+#endif
+
+- (IBAction)onNumButtonPressed:(id)sender
+{
+    NSString *ch = nil;
+    int len;
+
+    if (sender == button_Clear) {
+        [value setString:@""];
+    }
+    else if (sender == button_BS) {
+        // バックスペース
+        len = value.length;
+        if (len > 0) {
+            [value deleteCharactersInRange:NSMakeRange(len-1, 1)];
+        }
+    }
+		
+    else if (sender == button_0) ch = @"0";
+    else if (sender == button_1) ch = @"1";
+    else if (sender == button_2) ch = @"2";
+    else if (sender == button_3) ch = @"3";
+    else if (sender == button_4) ch = @"4";
+    else if (sender == button_5) ch = @"5";
+    else if (sender == button_6) ch = @"6";
+    else if (sender == button_7) ch = @"7";
+    else if (sender == button_8) ch = @"8";
+    else if (sender == button_9) ch = @"9";
+
+    if (ch != nil) {
+        [value appendString:ch];
+    }
+	
+    len = value.length;
+    NSMutableString *p = [[NSMutableString alloc] initWithCapacity:len];
+    for (int i = 0; i < len; i++) {
+        [p appendString:@"●"];
+    }
+    valueLabel.text = p;
+    [p release];
+}
+
+- (void)doneAction:(id)sender
+{
+    [delegate pinViewFinished:self isCancel:NO];
+
+    [value setString:@""];
+    valueLabel.text = @"";
+}
+
+- (void)cancelAction:(id)sender
+{
+    [delegate pinViewFinished:self isCancel:YES];
+
+    [value setString:@""];
+    valueLabel.text = @"";
+}
+
+@end
